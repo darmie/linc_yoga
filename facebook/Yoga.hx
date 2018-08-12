0b3e30eb89ea1796d138991cc3418998caca5ad9
dimous <kasimowsky@gmail.com>
2018-08-12 19:43:45 +1000

    Added flex bindings

--- facebook/Yoga.hx
+++ facebook/Yoga.hx
@@ -160,8 +160,259 @@ extern class Yoga {
 	@:native('::YGNodeLayoutGetDidUseLegacyFlag')
 	public static function nodeLayoutGetDidUseLegacyFlag(node:Node):Bool;
 
+	
+	@:native('::YGNodeStyleSetDirection')
+	public static function nodeStyleSetDirection(node:Node, direction:Direction):Void;
+
+	@:native('::YGNodeStyleGetDirection')
+	public static function nodeStyleGetDirection(node:Node):Direction;
+
+
+	@:native('::YGNodeStyleSetFlexDirection')
+	public static function nodeStyleSetFlexDirection(node:Node, flexDirection:FlexDirection):Void;
+
+	@:native('::YGNodeStyleGetFlexDirection')
+	public static function nodeStyleGetFlexDirection(node:Node):FlexDirection;
+
+
+	@:native('::YGNodeStyleSetJustifyContent')
+	public static function nodeStyleSetJustifyContent(node:Node, justifyContent:Justify):Void;
+
+	@:native('::YGNodeStyleGetJustifyContent')
+	public static function nodeStyleGetJustifyContent(node:Node):Justify;
+
+
+	@:native('::YGNodeStyleSetAlignContent')
+	public static function nodeStyleSetAlignContent(node:Node, alignContent:Align):Void;
+
+ 	@:native('::YGNodeStyleGetAlignContent')
+	public static function nodeStyleGetAlignContent(node:Node):Align;
+
+
+	@:native('::YGNodeStyleSetAlignItems')
+	public static function nodeStyleSetAlignItems(node:Node, alignItems:Align):Void;
+
+	@:native('::YGNodeStyleGetAlignItems')
+	public static function nodeStyleGetAlignItems(node:Node):Align;
+
+
+	@:native('::YGNodeStyleSetAlignSelf')
+	public static function nodeStyleSetAlignSelf(node:Node, alignSelf:Align):Void;
+	
+	@:native('::YGNodeStyleGetAlignSelf')
+	public static function nodeStyleGetAlignSelf(node:Node):Align;
+
+
+	@:native('::YGNodeStyleSetPositionType')
+	public static function nodeStyleSetPositionType(node:Node, positionType:PositionType):Void;
+
+	@:native('::YGNodeStyleGetPositionType')
+	public static function nodeStyleGetPositionType(node:Node):PositionType;
+
+
+	@:native('::YGNodeStyleSetFlexWrap')
+	public static function nodeStyleSetFlexWrap(node:Node, flexWrap:FlxWrap):Void;
+
+	@:native('::YGNodeStyleGetFlexWrap')
+	public static function nodeStyleGetFlexWrap(node:Node):FlxWrap;
+
+
+	@:native('::YGNodeStyleSetOverflow')
+	public static function nodeStyleSetOverflow(node:Node, overflow:Overflow):Void;
+
+	@:native('::YGNodeStyleGetOverflow')
+	public static function nodeStyleGetOverflow(node:Node):Overflow;
+
+
+	@:native('::YGNodeStyleSetDisplay')
+	public static function nodeStyleSetDisplay(node:Node, display:Display):Void;
+
+	@:native('::YGNodeStyleGetDisplay')
+	public static function nodeStyleGetDisplay(node:Node):Display;
+
+
+	@:native('::YGNodeStyleSetFlex')
+	public static function nodeStyleSetFlex(node:Node, flex:Float):Void;
+
+	@:native('::YGNodeStyleGetFlex')
+	public static function nodeStyleGetFlex(node:Node):Float;
+
+
+	@:native('::YGNodeStyleSetFlexGrow')
+	public static function nodeStyleSetFlexGrow(node:Node, flexGrow:Float):Void;
+
+	@:native('::YGNodeStyleGetFlexGrow')
+	public static function nodeStyleGetFlexGrow(node:Node):Float;
+
+
+	@:native('::YGNodeStyleSetFlexShrink')
+	public static function nodeStyleSetFlexShrink(node:Node, flexShrink:Float):Void;
+
+	@:native('::YGNodeStyleGetFlexShrink')
+	public static function nodeStyleGetFlexShrink(node:Node):Float;
+
+
+	@:native('::YGNodeStyleSetFlexBasis')
+	public static function nodeStyleSetFlexBasis(node:Node, flexBasis:Float):Void;
+
+	@:native('::YGNodeStyleSetFlexBasisPercent')
+	public static function nodeStyleSetFlexBasisPercent(node:Node, flexBasis:Float):Void;
+
+	@:native('::YGNodeStyleSetFlexBasisAuto')
+	public static function nodeStyleSetFlexBasisAuto(node:Node):Void;
+
+	@:native('::YGNodeStyleGetFlexBasis')
+	public static function nodeStyleGetFlexBasis(node:Node):Value;	
+
+
+	@:native('::YGNodeStyleSetPosition')
+	public static function nodeStyleSetPosition(node:Node, edge:Edge, position:Float):Void;
+
+	@:native('::YGNodeStyleSetPositionPercent')
+	public static function nodeStyleSetPositionPercent(node:Node, edge:Edge, position:Float):Void;
+
+	@:native('::YGNodeStyleGetPosition')
+	public static function nodeStyleGetPosition(node:Node, edge:Edge):Value;
+
+
+	@:native('::YGNodeStyleSetMargin')
+	public static function nodeStyleSetMargin(node:Node, edge:Edge, margin:Float):Void;
+
+	@:native('::YGNodeStyleSetMarginPercent')
+	public static function nodeStyleSetMarginPercent(node:Node, edge:Edge, margin:Float):Void;
+
+	@:native('::YGNodeStyleSetMarginAuto')
+	public static function nodeStyleSetMarginAuto(node:Node, edge:Edge):Void;
+
+	@:native('::YGNodeStyleGetMargin')
+	public static function nodeStyleGetMargin(node:Node, edge:Edge):Value;
+
+
+	@:native('::YGNodeStyleSetPadding')
+	public static function nodeStyleSetPadding(node:Node, edge:Edge, padding:Float):Void;
+
+	@:native('::YGNodeStyleSetPaddingPercent')
+	public static function nodeStyleSetPaddingPercent(node:Node, edge:Edge, padding:Float):Void;
+
+	@:native('::YGNodeStyleGetPadding')
+	public static function nodeStyleGetPadding(node:Node, edge:Edge):Value;
+
+
+	@:native('::YGNodeStyleSetBorder')
+	public static function nodeStyleSetBorder(node:Node, edge:Edge, border:Float):Void;
+
+	@:native('::YGNodeStyleGetBorder')
+	public static function nodeStyleGetBorder(node:Node, edge:Edge):Float;
+
+
+	@:native('::YGNodeStyleSetWidth')
+	public static function nodeStyleSetWidth(node:Node, width:Float):Void;
+
+	@:native('::YGNodeStyleSetWidthPercent')
+	public static function nodeStyleSetWidthPercent(node:Node, width:Float):Void;
+
+	@:native('::YGNodeStyleSetWidthAuto')
+	public static function nodeStyleSetWidthAuto(node:Node):Void;
+
+	@:native('::YGNodeStyleGetWidth')
+	public static function nodeStyleGetWidth(node:Node):Value;
+
+
+	@:native('::YGNodeStyleSetHeight')
+	public static function nodeStyleSetHeight(node:Node, height:Float):Void;
+
+	@:native('::YGNodeStyleSetHeightPercent')
+	public static function nodeStyleSetHeightPercent(node:Node, height:Float):Void;
+
+	@:native('::YGNodeStyleSetHeightAuto')
+	public static function nodeStyleSetHeightAuto(node:Node):Void;
+
+	@:native('::YGNodeStyleGetHeight')
+	public static function nodeStyleGetHeight(node:Node):Value;
+
+
+	@:native('::YGNodeStyleSetMinWidth')
+	public static function nodeStyleSetMinWidth(node:Node, minWidth:Float):Void;
+
+	@:native('::YGNodeStyleSetMinWidthPercent')
+	public static function nodeStyleSetMinWidthPercent(node:Node, minWidth:Float):Void;
+
+	@:native('::YGNodeStyleGetMinWidth')
+	public static function nodeStyleGetMinWidth(node:Node):Value;
+
+
+	@:native('::YGNodeStyleSetMinHeight')
+	public static function nodeStyleSetMinHeight(node:Node, minHeight:Float):Void;
+
+	@:native('::YGNodeStyleSetMinHeightPercent')
+	public static function nodeStyleSetMinHeightPercent(node:Node, minHeight:Float):Void;
+
+	@:native('::YGNodeStyleGetMinHeight')
+	public static function nodeStyleGetMinHeight(node:Node):Value;
+
+
+	@:native('::YGNodeStyleSetMaxWidth')
+	public static function nodeStyleSetMaxWidth(node:Node, maxWidth:Float):Void;
+
+	@:native('::YGNodeStyleSetMaxWidthPercent')
+	public static function nodeStyleSetMaxWidthPercent(node:Node, maxWidth:Float):Void;
+
+	@:native('::YGNodeStyleGetMaxWidth')
+	public static function nodeStyleGetMaxWidth(node:Node):Value;
+
+
+	@:native('::YGNodeStyleSetMaxHeight')
+	public static function nodeStyleSetMaxHeight(node:Node, maxHeight:Float):Void;
+
+	@:native('::YGNodeStyleSetMaxHeightPercent')
+	public static function nodeStyleSetMaxHeightPercent(node:Node, maxHeight:Float):Void;
+
+	@:native('::YGNodeStyleGetMaxHeight')
+	public static function nodeStyleGetMaxHeight(node:Node):Value;
+ 
+
+	@:native('::YGNodeStyleSetAspectRatio')
+	public static function nodeStyleSetAspectRatio(node:Node, aspectRatio:Float):Void;
+
+	@:native('::YGNodeStyleGetAspectRatio')
+	public static function nodeStyleGetAspectRatio(node:Node):Float;
+
+
+	@:native('::YGNodeLayoutGetLeft')
+	public static function nodeLayoutGetLeft(node:Node):Float;
+
+	@:native('::YGNodeLayoutGetTop')
+	public static function nodeLayoutGetTop(node:Node):Float;
+
+	@:native('::YGNodeLayoutGetRight')
+	public static function nodeLayoutGetRight(node:Node):Float;
+
+	@:native('::YGNodeLayoutGetBottom')
+	public static function nodeLayoutGetBottom(node:Node):Float;
+
+	@:native('::YGNodeLayoutGetWidth')
+	public static function nodeLayoutGetWidth(node:Node):Float;
+
+	@:native('::YGNodeLayoutGetHeight')
+	public static function nodeLayoutGetHeight(node:Node):Float;
+
+	@:native('::YGNodeLayoutGetDirection')
+	public static function nodeLayoutGetDirection(node:Node):Direction;
+
+	@:native('::YGNodeLayoutGetHadOverflow')
+	public static function nodeLayoutGetHadOverflow(node:Node):Bool;
+
 	@:native('::YGNodeLayoutGetDidLegacyStretchFlagAffectLayout')
 	public static function nodeLayoutGetDidLegacyStretchFlagAffectLayout(node:Node):Bool;
+
+	@:native('::YGNodeLayoutGetMargin')
+	public static function nodeLayoutGetMargin(node:Node, edge:Edge):Float;
+
+	@:native('::YGNodeLayoutGetBorder')
+	public static function nodeLayoutGetBorder(node:Node, edge:Edge):Float;
+
+	@:native('::YGNodeLayoutGetPadding')
+	public static function nodeLayoutGetPadding(node:Node, edge:Edge):Float;
 
 
 	@:native('::YGConfigSetLogger')
